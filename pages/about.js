252224ca84986bb69dbc2387b39c8447406fb9ca
coevoai <coevoai@gmail.com>
2024-12-01 22:21:06 +0900

    ✨ ヘッダーの追加

--- pages/about.js
+++ pages/about.js
@@ -1,12 +1,14 @@
 import { Footer } from "@/components/Footer";
 import { Header } from "@/components/Header";
+import { HeadMeta } from "@/components/HeadMeta";
 import { Main } from "@/components/Main";
 import { Typography } from "@/components/Typography";
 
 export default function Home() {
   return (
     <>
-      <Header title="About Page" />
+      <HeadMeta title="About Page" />
+      <Header />
       <Typography>
         <Main page="about" />
         <Footer />
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
 import { Footer } from "@/components/Footer";
 import { Header } from "@/components/Header";
+import { HeadMeta } from "@/components/HeadMeta";
 import { Main } from "@/components/Main";
 import { Typography } from "@/components/Typography";
 
 export default function Home() {
   return (
     <>
-      <Header title="Index Page" />
+      <HeadMeta title="Index Page" />
+      <Header />
       <Typography>
         <Main page="index" />
         <Footer />
