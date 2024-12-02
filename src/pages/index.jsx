a27043e340f084c1b25661c4c7d8ce8f58469b0a
coevoai <coevoai@gmail.com>
2024-12-02 18:14:08 +0900

    ✨ useStateで文字列と真偽値を追加

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -7,6 +7,8 @@ import { useCallback, useEffect, useState } from "react";
 
 export default function Home() {
   const [count, setCount] = useState(1);
+  const [text, setText] = useState("");
+  const [isShow, setIsShow] = useState(true);
 
   const handleClick = useCallback(() => {
     if (count < 10) {
@@ -14,12 +16,24 @@ export default function Home() {
     }
   }, [count]);
 
+  const handleChange = useCallback((e) => {
+    if (e.target.value.length > 5) {
+      alert("5文字以内にしてください");
+      return;
+    }
+    setText(e.target.value.trim());
+  }, []);
+
+  const handleDisplay = useCallback(() => {
+    setIsShow((isShow) => !isShow)
+  }, []);
+
   useEffect(() => {
     document.body.style.backgroundColor = "lightblue";
     return () => {
       document.body.style.backgroundColor = "";
     }
-  }, [])
+  }, []);
 
   return (
     <>
@@ -29,8 +43,12 @@ export default function Home() {
       </Head>
       <Header />
       <Typography>
-        <h1>{count}</h1>
+        {isShow ? <h1>{count}</h1> : null}
         <button onClick={handleClick}>ボタン</button>
+        <button onClick={handleDisplay}
+        >
+          {isShow ? "非表示" : "表示"}</button>
+        <input type="text" value={text} onChange={handleChange} />
         <Main page="index" />
         <Footer />
       </Typography>
