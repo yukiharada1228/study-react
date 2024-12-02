eb3ea2017e8628c21b0641db27cdd260f530305a
coevoai <coevoai@gmail.com>
2024-12-02 09:34:49 +0900

    ⚡️ useCallbackとuseEffect

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,15 +3,16 @@ import { Footer } from "@/src/components/Footer";
 import { Header } from "@/src/components/Header";
 import { Main } from "@/src/components/Main";
 import { Typography } from "@/src/components/Typography";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 export default function Home() {
   const [count, setCount] = useState(1);
 
-  const handleClick = (e) => {
-    setCount(count => count + 1);
-    setCount(count => count + 1);
-  };
+  const handleClick = useCallback(() => {
+    if (count < 10) {
+      setCount(count => count + 1);
+    }
+  }, [count]);
 
   useEffect(() => {
     document.body.style.backgroundColor = "lightblue";
